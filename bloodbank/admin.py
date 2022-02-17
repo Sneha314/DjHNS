bb20e82e90d6f38c2fe184c94713ac5e0cec98e8
aflah <aflahvk2527@gmail.com>
2022-02-17 12:41:06 +0530

    Phase1

--- bloodbank/admin.py
+++ bloodbank/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import BloodDonor
+from .models import BloodDonor, BloodGroups
 
 admin.site.register(BloodDonor)
+admin.site.register(BloodGroups)
--- bloodbank/models.py
+++ bloodbank/models.py
@@ -2,11 +2,17 @@ from django.db import models
 
 # Create your models here.
 
+class BloodGroups(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
 class BloodDonor(models.Model):
     name = models.CharField(max_length=30)
     age = models.IntegerField()
     place = models.CharField(max_length=50)
-    blood_group = models.CharField(max_length=20)
+    blood_group = models.ForeignKey(BloodGroups, on_delete=models.SET_DEFAULT, null=True, default="covishield")
     
     def __str__(self):
         return self.name
